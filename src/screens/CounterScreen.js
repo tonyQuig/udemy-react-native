2c0064fcaca961e25560e568329843a3d127e24c
tonyquig <tquigley9@gmail.com>
2020-03-15 10:57:58 +0000

    Added useReducer to handle counter updates on CounterScreen

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,14 +1,25 @@
-import React, { useState } from "react"
+import React, { useReducer } from "react"
 import { Text, View, StyleSheet, Button } from "react-native"
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case "change_increment":
+      return {...state, counter: state.counter + action.payload};
+    case "change_decrement":
+      return {...state, counter: state.counter - action.payload};
+    default:
+      return state;
+  }
+}
 
 const CounterScreen = () => {
 
-  const [counter, setCounter] = useState(0)
+  const [state, dispatch] = useReducer(reducer, {counter: 0})
 
   const handleCounter = (operation) => {
-    operation === "increment" ? 
-      setCounter(prev => prev + 1) :
-      setCounter(prev => prev - 1)
+    operation === "increment" ?
+      dispatch({type: "change_increment", payload: 1}) :
+      dispatch({type: "change_decrement", payload: 1})
   }
 
   return(
@@ -22,7 +33,7 @@ const CounterScreen = () => {
         onPress={() => handleCounter("decrement")} 
       />
 
-      <Text>Current Counter: {counter}</Text>
+      <Text>Current Counter: {state.counter}</Text>
 
     </View>
   )
